5b210c8f44d2d64a2d879f441cf9459f814172e0
Rohith Reddy Sama <36402548+samarohith@users.noreply.github.com>
2023-02-27 15:27:12 +0530

    Add files via upload

---
+++ muta/script.py
@@ -0,0 +1,16 @@
+import matplotlib.pyplot as plt
+
+datax = []
+datay = []
+for line in open("array_info.txt"):
+    values = line.split()
+    datax.append(int(values[0]))
+    datay.append(float(values[1]))
+
+# for x, y in zip(datax, datay):
+#     print(x, "-" ,y)
+fig, ax = plt.subplots()
+ax.scatter(datax, datay)
+ax.locator_params(integer=True)
+ax.set(xlim=(94,100), ylim=(0,10))
+plt.show()
